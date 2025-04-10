c016cec9d18627c702264b5bd9575b7e7b8afc47
Fabrizzio <fabrizziobravo@gmail.com>
2025-04-10 11:50:26 -0500

    ejercicio01

---
+++ Fundamentos de algoritmos/semana3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio01();
+            Console.ReadKey();
+        }
+        static void ejercicio01()
+        {
+            string nombre;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine(nombre+" Bienvenido al curso de Fundamento De Algoritmos");
+
+
+        }
+    }
+}
